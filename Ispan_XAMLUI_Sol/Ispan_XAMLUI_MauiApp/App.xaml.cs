bbc11d346104c6588136513878f7f3e8d00e398e
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-02-03 10:52:45 +0800

    add example

---
+++ Ispan_XAMLUI_Sol/Ispan_XAMLUI_MauiApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Ispan_XAMLUI_MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            //MainPage = new MainPage();
+        }
+    }
+}
---
+++ Ispan_XAMLUI_Sol/Ispan_XAMLUI_MauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ispan_XAMLUI_MauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
