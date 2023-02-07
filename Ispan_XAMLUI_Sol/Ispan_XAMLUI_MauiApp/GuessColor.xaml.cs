079e6b184cd3390baf5830a2f31d77239975875d
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-02-08 01:08:25 +0800

    ReArrange MainPage

---
+++ Ispan_XAMLUI_Sol/Ispan_XAMLUI_MauiApp/GuessColor.xaml.cs
@@ -0,0 +1,14 @@
+namespace Ispan_XAMLUI_MauiApp;
+
+public partial class GuessColor : ContentPage
+{
+	public GuessColor()
+	{
+		InitializeComponent();
+	}
+
+    private void WashCardClicked(object sender, EventArgs e)
+    {
+
+    }
+}
